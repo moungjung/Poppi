fce49c32789d11edecdf9f0f3d1a1aa060d5db7b
Moungjung Kim <moungjung@gmail.com>
2020-04-30 14:50:00 -0600

    Initial Project Setup

---
+++ Poppi/Poppi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Poppi.Startup))]
+namespace Poppi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
