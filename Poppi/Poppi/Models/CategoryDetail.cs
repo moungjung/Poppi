a454f3df77f58ae2cf16686b3a4876af6cb88041
Moungjung Kim <moungjung@gmail.com>
2020-05-01 13:11:09 -0600

    Added database into solution

---
+++ Poppi/Poppi/Models/CategoryDetail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Poppi.Models
+{
+    public class CategoryDetail
+    {
+        public int CategoryID { get; set; }
+        [Required(ErrorMessage ="Category Name Required")]
+        [StringLength(100, ErrorMessage = "Minimum 3 and minimum 5 and maximum 100 characters")]
+        public string CategoryName { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+    }
+
+    public class ProductDetail
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int AmountOfChamps { get; set; }
+        public string Region { get; set; }
+        public string ProductImage { get; set; }
+        public Nullable<bool> IsFeatured { get; set; }
+        public int AmountOfSkins { get; set; }
+        public string SkinDetails { get; set; }
+        public string ExtraAccountDetails { get; set; }
+        public int CategoryID { get; set; }
+        [Range(typeof(decimal), "1", "200000", ErrorMessage ="invalid price")]
+        public Nullable<decimal> Price { get; set; }
+    }
+}
---
+++ Poppi/Poppi/Models/Shopping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Poppi.Models
+{
+    public class Shippingdetail
+    {
+        public int ShippingDetailID { get; set; }
+        [Required]
+        public Nullable<int> MemberID { get; set; }
+        [Required]
+        public string ShippingEmailAddress { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+        [Required]
+        public decimal AmountPaid { get; set; }
+        [Required]
+        public string PaymentType { get; set; }
+    }
+}
